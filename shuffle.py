da1899977c8b51d6336746d0cfe1fafe1d5932cd
VladimirCharkot <vlad.chk@gmail.com>
2020-02-09 20:19:59 -0300

    shuffle comentado

--- shuffle.py
+++ shuffle.py
@@ -1,22 +1,29 @@
 import random
 
-print("Ingresame una frase wacho")
-print("> ",end='')
+# Si uno empieza una línea con el
+# símbolo # entonces el intérprete
+# ignora esa línea
 
+# A estas líneas se las llama comentarios
+# y sirven para documentar y anotar el código
+
+# Función 'mezclar': recibe una frase en str,
+# y la devuelve con las palabras mezcladas y
+# separadas por guiones, tipo 'str'
 def mezclar(frase):
     separador = '-'
-    frasli = frase.split()
-    random.shuffle(frasli)
-    palabras = separador.join(frasli)
+    frase_lista = frase.split()
+    random.shuffle(frase_lista)     # esta instrucción la mezcla
+    palabras = separador.join(frase_lista)
     return palabras
 
-entrada = input()
-primer = mezclar(entrada)
 
-print("Primer shuffle: ")
-print(primer)
-print()
+# Pedimos entrada del teclado
+# El valor ingreso termina en la
+# variable 'entrada', y con tipo str
+print("Ingresá una frase")
+print("> ",end='')
+frase_ingresada = input()
 
-print("Los cinco:")
-for i in range(5):
-    print(mezclar(entrada))
+frase_mezclada = mezclar(frase_ingresada)
+print(frase_mezclada)
